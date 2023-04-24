dfca3c8acb8e6ca524456e67adcf62ad939a5f88
Roberto_Campo <alexserjs@gmail.com>
2023-04-24 12:17:55 +0200

    3ra Prueba

--- src/ut8/Ejercicio01/Ejercicio01.java
+++ src/ut8/Ejercicio01/Ejercicio01.java
@@ -3,5 +3,16 @@ package ut8.Ejercicio01;
 public class Ejercicio01 {
 public static void main(String[] args) {
 	
+	System.out.println("holaMundo");
+	
+	switch (key) {
+	case value: {
+		
+		yield type;
+	}
+	default:
+		throw new IllegalArgumentException("Unexpected value: " + key);
+	}
+	
 }
 }
